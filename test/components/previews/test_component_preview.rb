201a5688d6efe5ae5d9d11a95f3a56e371b1c783
Mixer Gutierrez <mixergutierrez@gmail.com>
2020-07-31 19:11:27 -0500

    Disable monkey patch for render and replace render with render_component

--- test/components/previews/test_component_preview.rb
+++ test/components/previews/test_component_preview.rb
@@ -1,14 +1,14 @@
 class TestComponentPreview < ViewComponent::Preview
   def with_default_title
-    render(TestComponent.new(title: "Test component default")) { "Hello" }
+    render_component(TestComponent.new(title: "Test component default")) { "Hello" }
   end
 
   def with_long_title
-    render(TestComponent.new(title: "This is a really long title to see how the component renders this"))
+    render_component(TestComponent.new(title: "This is a really long title to see how the component renders this"))
   end
 
   def with_content_block
-    render(TestComponent.new(title: "This component accepts a block of content")) do
+    render_component(TestComponent.new(title: "This component accepts a block of content")) do
       tag.div do
         content_tag(:span, "Hello")
       end
